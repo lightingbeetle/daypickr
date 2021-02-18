cad8f9b809efe73dca9a8ef12e0c10290ea3db76
jurajk <juraj.kiss@gmail.com>
2021-02-18 17:03:06 +0100

    make selects and buttons clickable

--- src/daypicker.js
+++ src/daypicker.js
@@ -22,20 +22,6 @@ const defaultConfig = {
     isToday: "isToday",
     srOnly: "sr-only",
   },
-  months: [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December",
-  ],
   years: [
     "1990",
     "1991",
@@ -54,10 +40,24 @@ class Daypicker {
   constructor(config) {
     this.config = { ...defaultConfig, ...config };
 
-    this.init();
-
     this.today = new Date();
+    this.currentView = {
+      year: this.today.getFullYear(),
+      month: this.today.getMonth(),
+    };
     this.currentViewDates = [];
+
+    this.yearSelect = null;
+    this.monthSelect = null;
+    this.prevButton = null;
+    this.nextButton = null;
+
+    this.onYearChange = this.onYearChange.bind(this);
+    this.onMonthChange = this.onMonthChange.bind(this);
+    this.onPrevMonth = this.onPrevMonth.bind(this);
+    this.onNextMonth = this.onNextMonth.bind(this);
+
+    this.init();
 
     return this;
   }
@@ -75,9 +75,10 @@ class Daypicker {
     this.identifier = this.boundElement.getAttribute("id");
 
     this.arrangeWeekdays();
-    this.populateCalendar(2020, 11);
+    this.populateCalendar();
     this.renderDialog();
     this.renderCalendar();
+    this.bindNavigation();
   }
 
   renderDialog() {
@@ -92,6 +93,7 @@ class Daypicker {
     );
 
     this.dialog = document.getElementById(dialogId);
+    console.log(this.dialog);
   }
 
   renderCalendar() {
@@ -110,8 +112,12 @@ class Daypicker {
     this.calendar = document.getElementById(calendarId);
   }
 
-  populateCalendar(year, month) {
-    const days = getMonthView(year, month, this.config.firstDayOfWeek);
+  populateCalendar() {
+    const days = getMonthView(
+      this.currentView.year,
+      this.currentView.month,
+      this.config.firstDayOfWeek
+    );
 
     this.currentViewDates = days.map((day) => ({
       date: day,
@@ -125,8 +131,66 @@ class Daypicker {
       0,
       this.config.firstDayOfWeek
     );
-    console.log(moveToEnd);
     this.config.l10n.weekdays = [...this.config.l10n.weekdays, ...moveToEnd];
+  }
+
+  bindNavigation() {
+    this.yearSelect = this.dialog.querySelector("[data-daypicker-year-select]");
+    this.monthSelect = this.dialog.querySelector(
+      "[data-daypicker-month-select]"
+    );
+    this.prevButton = this.dialog.querySelector("[data-daypicker-prev-month]");
+    this.nextButton = this.dialog.querySelector("[data-daypicker-next-month]");
+
+    this.yearSelect.addEventListener("change", this.onYearChange);
+    this.monthSelect.addEventListener("change", this.onMonthChange);
+    this.prevButton.addEventListener("click", this.onPrevMonth);
+    this.nextButton.addEventListener("click", this.onNextMonth);
+  }
+
+  onYearChange() {
+    this.calendar.remove();
+    this.currentView.year = parseInt(this.yearSelect.value, 10);
+    this.populateCalendar();
+    this.renderCalendar();
+  }
+
+  onMonthChange() {
+    this.calendar.remove();
+    this.currentView.month = parseInt(this.monthSelect.value, 10);
+    this.populateCalendar();
+    this.renderCalendar();
+  }
+
+  onPrevMonth() {
+    if (this.currentView.month > 0) {
+      this.currentView.month = this.currentView.month - 1;
+    } else {
+      this.currentView.month = 11;
+      this.currentView.year = this.currentView.year - 1;
+    }
+    this.updateSelects();
+    this.calendar.remove();
+    this.populateCalendar();
+    this.renderCalendar();
+  }
+
+  onNextMonth() {
+    if (this.currentView.month < 11) {
+      this.currentView.month = this.currentView.month + 1;
+    } else {
+      this.currentView.month = 0;
+      this.currentView.year = this.currentView.year + 1;
+    }
+    this.updateSelects();
+    this.calendar.remove();
+    this.populateCalendar();
+    this.renderCalendar();
+  }
+
+  updateSelects() {
+    this.monthSelect.value = this.currentView.month;
+    this.yearSelect.value = this.currentView.year;
   }
 }
 
--- src/templates/dialog.js
+++ src/templates/dialog.js
@@ -1,29 +1,29 @@
-export default ({
-  classes,
-  l10n,
-  months,
-  years,
-  id,
-}) => `
+export default ({ classes, l10n, years, id }) => `
   <div id="${id}" class="${classes.wrapper}" role="dialog" aria-modal="true">
     <div class="${classes.header}">
 
       <label>
         ${l10n.month}
-        <select data-daypicker-month-select class="${classes.select} ${classes.monthSelect}">
-          ${months.map(month => `<option>${month}</option>`)}
+        <select data-daypicker-month-select class="${classes.select} ${
+  classes.monthSelect
+}">
+          ${l10n.months.map((m, i) => `<option value=${i}>${m}</option>`)}
         </select>
       </label>
 
       <label>
         ${l10n.year}
-        <select data-daypicker-year-select class="${classes.select} ${classes.yearSelect}">
-          ${years.map(year => `<option>${year}</option>`)}
+        <select data-daypicker-year-select class="${classes.select} ${
+  classes.yearSelect
+}">
+          ${years.map((year) => `<option>${year}</option>`)}
         </select>
       </label>
 
       <button type="button" data-daypicker-prev-month>${l10n.prevMonth}</button>
-      <button type="button" data-daypicker-prev-month>${l10n.nextMonth}</button>  
+      <button type="button" data-daypicker-next-month>${
+        l10n.nextMonth
+      }</button>  
     </div>
   </div>
 `;
