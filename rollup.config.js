870486ab13390fc48675c3771e3fa4942e4c647b
jurajk <juraj.kiss@gmail.com>
2021-02-19 11:54:00 +0100

    add browsersync, ditch serve

--- rollup.config.js
+++ rollup.config.js
@@ -1,21 +1,23 @@
-import resolve from '@rollup/plugin-node-resolve';
-import commonjs from '@rollup/plugin-commonjs';
-import { terser } from 'rollup-plugin-terser';
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import { terser } from "rollup-plugin-terser";
+import browsersync from "rollup-plugin-browsersync";
 
 // `npm run build` -> `production` is true
 // `npm run dev` -> `production` is false
 const production = !process.env.ROLLUP_WATCH;
 
 export default {
-  input: 'src/daypicker.js',
+  input: "src/daypicker-preact.js",
   output: {
-    file: 'public/daypicker.js',
-    format: 'iife', // immediately-invoked function expression — suitable for <script> tags
-    sourcemap: true
+    file: "public/daypicker-preact.js",
+    format: "iife", // immediately-invoked function expression — suitable for <script> tags
+    sourcemap: true,
   },
   plugins: [
     resolve(), // tells Rollup how to find date-fns in node_modules
     commonjs(), // converts date-fns to ES modules
-    production && terser() // minify, but only in production
-  ]
+    browsersync({ server: "public", files: ["public/*"] }),
+    production && terser(), // minify, but only in production
+  ],
 };
