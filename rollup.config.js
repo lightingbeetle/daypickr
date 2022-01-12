e406fdfc32a82d4cffc951ed806298114c819cf0
jurajk <juraj.kiss@gmail.com>
2022-01-12 16:46:34 +0100

    don't run browsersync in prod, pretty

--- rollup.config.js
+++ rollup.config.js
@@ -1,26 +1,26 @@
-import resolve from "@rollup/plugin-node-resolve";
-import commonjs from "@rollup/plugin-commonjs";
-import { terser } from "rollup-plugin-terser";
-import browsersync from "rollup-plugin-browsersync";
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import { terser } from 'rollup-plugin-terser';
+import browsersync from 'rollup-plugin-browsersync';
 
 // `npm run build` -> `production` is true
 // `npm run dev` -> `production` is false
 const production = !process.env.ROLLUP_WATCH;
 
 export default {
-  input: "src/daypicker-preact.js",
+  input: 'src/daypicker-preact.js',
   output: {
-    file: "public/daypicker-preact.js",
-    format: "iife", // immediately-invoked function expression — suitable for <script> tags
+    file: 'public/daypicker-preact.js',
+    format: 'cjs',
     sourcemap: true,
   },
   plugins: [
     resolve(), // tells Rollup how to find date-fns in node_modules
     commonjs(), // converts date-fns to ES modules
-    browsersync({ server: "public", files: ["public/*"] }),
+    !production && browsersync({ server: 'public', files: ['public/*'] }),
     production && terser(), // minify, but only in production
   ],
   watch: {
     include: 'src/**',
-  }
+  },
 };
