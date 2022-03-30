abe0814d002c5962dd0d1838954603b3d64e9d94
jurajk <juraj.kiss@gmail.com>
2022-03-30 15:17:45 +0200

    fix month select

--- src/components/MonthSelect.js
+++ src/components/MonthSelect.js
@@ -11,7 +11,7 @@ const MonthSelect = () => {
     label=${l10n.month}
     value=${view.getMonth()}
     className=${classes.monthSelect}
-    options=${l10n.months.map((month, i) => ({ value: i, text: month }))}
+    options=${l10n.months.map((month, i) => ({ value: i.toString(), text: month }))}
     onChange=${(e) => {
       const newView = new Date(view);
       newView.setMonth(parseInt(e.target.value, 10));
