7a04786e39d31018b66b8da23245509ae18aa437
jurajk <juraj.kiss@gmail.com>
2022-03-30 14:38:35 +0200

    add default render scenarios

--- src/tests/render.test.js
+++ src/tests/render.test.js
@@ -1,10 +1,28 @@
 import { html } from 'htm/preact';
-import { render } from '@testing-library/preact';
+import { render, screen } from '@testing-library/preact';
 
 import Daypicker from '../components/Daypicker';
-import { isToday } from '../utils/date';
 
-test('renders', () => {
-  const { container } = render(`<${Daypicker} />`);
-  expect(container).toBeInTheDocument();
+describe('render', () => {
+  const example = html`<${Daypicker} />`;
+
+  test('initial', () => {
+    const { container } = render(example);
+    expect(container).toBeInTheDocument();
+  });
+
+  test('dialog', () => {
+    render(example);
+    const dialog = screen.getByRole('dialog', { hidden: true });
+    expect(dialog).toBeInTheDocument();
+  });
+
+  test('default day is today', () => {
+    expect.assertions(2);
+    const { container } = render(example);
+    const todayButton = container.querySelector('.isToday');
+
+    expect(todayButton.getAttribute('aria-label')).toBe(new Date().toLocaleDateString());
+    expect(todayButton.getAttribute('tabindex')).toBe('0');
+  });
 });
